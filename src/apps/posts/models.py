e1eaa44a20de08deee43eaf12e7b364fd3460226
duckraper <klyman047@gmail.com>
2024-08-29 13:49:03 -0400

    posts model changed its FPK

--- src/apps/posts/models.py
+++ src/apps/posts/models.py
@@ -1,3 +1,16 @@
-from django.db import models
+from django.db import models as m
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Post(m.Model):
+    user = m.ForeignKey(to='users.User', on_delete=m.CASCADE, related_name='posts')
+    content = m.CharField(max_length=255)
+    image = m.ImageField(upload_to='posts/',
+                         help_text=_('Upload an image for the post to get in context'),
+                         blank=True)
+    archived = m.BooleanField(default=False)
+    created_at = m.DateTimeField(auto_now_add=True)
+    updated_at = m.DateTimeField()
+
+    def __str__(self):
+        return f'"{self.content[:50]}...{self.content[-50:]}" by @{self.user.username}'
